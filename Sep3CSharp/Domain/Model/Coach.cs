b49e4b70bd98e3b2d3f2dad439587177dca89859
blumenn <Anders.Blumensaat@gmail.com>
2023-08-01 14:23:09 +0200

    Add coach to team. Fixed the Date from string to a real date-object.

--- Sep3CSharp/Domain/Model/Coach.cs
+++ Sep3CSharp/Domain/Model/Coach.cs
@@ -4,10 +4,12 @@ public class Coach
 {
    public int Id { get; set; }
     public string fullName { get; set; }
-    public string birthday { get; set; }
+    public DateTime birthday { get; set; }
     public string initials { get; set; }
     public string email { get; set; }
     public string role { get; set; }
+    
+    public string teamName { get; set; }
     
    // public Team Team { get; set; }
 
